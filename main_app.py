78aacdd34e4004636767b9f2011c36144ea4c4f6
dillibabupec <dillibabu_it@panimalar.ac.in>
2024-06-27 14:28:28 +0530

    Add files via upload

---
+++ main_app.py
@@ -0,0 +1,44 @@
+#streamlit run main_app.py on cmd
+#Library imports
+#pip install streamlit/keras/tensorflow/opencv-python
+import numpy as np
+import streamlit as st
+import cv2
+from keras.models import load_model
+
+
+#Loading the Model
+model = load_model('model.h5')
+
+#Name of Classes
+CLASS_NAMES = ['normal','parkinson']
+
+#Setting Title of App
+st.title("Parkinsons Detection and Classification")
+st.markdown("Upload an image ")
+
+#Uploading the dog image
+dog_image = st.file_uploader("Choose an image...", type="png")
+submit = st.button('Predict')
+#On predict button click
+if submit:
+
+
+    if dog_image is not None:
+
+        # Convert the file to an opencv image.
+        file_bytes = np.asarray(bytearray(dog_image.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+
+
+
+        # Displaying the image
+        st.image(opencv_image, channels="BGR")
+        #Resizing the image
+        opencv_image = cv2.resize(opencv_image, (192,192))
+        #Convert image to 4 Dimension
+        opencv_image.shape = (1,192,192,3)
+        #Make Prediction
+        Y_pred = model.predict(opencv_image)
+
+        st.title(str(" Result is "+CLASS_NAMES[np.argmax(Y_pred)]))
